4d044cf39405538b1d25bc5f84b96c4cee3bf3e4
Yin, Jiayi <yinjiayi5@gmail.com>
2016-10-19 11:12:52 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    Scanner password = new Scanner(System.in);
+        System.out.println("Write your username: ");
+        String user = password.next();
+        System.out.println("Welcome, " + user);
+        Random rand = new Random();
+        int pass = rand.nextInt(2147483647)+ 32768;
+        System.out.println("Your password is: " + pass);
+        System.out.println("Would you like to log in: yes or no?");
+        String c = password.next();
+
+        if (c.equalsIgnoreCase("yes"))
+        {
+                System.out.println("Put in username and password: ");
+                String a = password.next();
+            if (a.equalsIgnoreCase(user + pass))
+                System.out.println("Welcome to your account");
+
+            else
+            System.out.println("Sorry try again");
+
+        }
+
+        else
+        {
+            System.exit(0);
+        }
+
     }
 }
